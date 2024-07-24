c9934249db18f7aed8ac6f5b23d07415432a12ce
jungeun <pje961011@gmail.com>
2024-07-24 16:14:32 +0900

    [Feat] db 연결 및 member 테이블 생성

--- src/main/webapp/db.sql
+++ src/main/webapp/db.sql
@@ -1,20 +1,20 @@
-create table menu_test
-(
-    m_no    number(3) primary key,
-    m_name  varchar2(20 char) not null,
-    m_price number(4) not null
+create table member(
+    m_id varchar2(50 char) primary key,
+    m_pw varchar2(50 char) not null,
+    m_fam_kanji varchar2(50 char) not null,
+    m_name_kanji varchar2(50 char) not null,
+    m_fam_kana varchar2(50 char) not null,
+    m_name_kana varchar2(50 char) not null,
+    m_fam_eng varchar2(50 char) not null,
+    m_name_eng varchar2(50 char) not null,
+    m_birth varchar2(30 char) not null,
+    m_gender varchar2(10 char) not null,
+    m_email varchar2(50 char) not null,
+    m_phone varchar2(20 char) not null,
+    m_address varchar2(100 char) not null,
+    m_zipcode varchar2(10 char) not null,
+    m_img varchar2(20 char) not null
 );
+select * from member;
+insert into member values ('test3', '1111', '山本', '光', 'ヤマモト', 'ヒカリ', 'yamamoto', 'hikari', '2000-01-01', '女', 'yamamoto@enparty.com', '000-0000-0000', '東京', '1234567', 'profile.jpg' )
 
-create sequence menu_test_seq;
-
-insert into menu_test
-values (menu_test_seq.nextval, '메뉴1', 1000);
-insert into menu_test
-values (menu_test_seq.nextval, '메뉴2', 2000);
-insert into menu_test
-values (menu_test_seq.nextval, '메뉴3', 3000);
-insert into menu_test
-values (menu_test_seq.nextval, '메뉴4', 4000);
-
-select *
-from menu_test;
