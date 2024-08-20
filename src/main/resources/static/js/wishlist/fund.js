efc3896ded69cf5bdd122a72fd29dc15f0eb2e91
STELLA\stell <leeteakoni@gmail.com>
2024-08-20 13:14:14 +0900

    [Feat] edited wishlist funding files

--- src/main/resources/static/js/wishlist/fund.js
+++ src/main/resources/static/js/wishlist/fund.js
@@ -6,10 +6,10 @@ let jsoninfos;
 $.ajax({
 	type: "post",
 	url: "/wishlist/funding",
-	data: { eno: eventno },
+	data: {eno: eventno},
 	async: false,
 	dataType: "json",
-	success: function(response) {
+	success: function (response) {
 		jsoninfos = response;
 		console.log(jsoninfos)
 		response.forEach((element, index) => {
@@ -28,104 +28,176 @@ $.ajax({
 					</div>
 				</div>`);
 		});
+
 		$(".kh-f-card-container").append(`<div class="kh-f-card-fake"></div>`)
-		let percent = response.percent;
-		if (percent === undefined) {
-			percent = 0;
-		}
+
+
 		//색상 등
+		// let card = document.querySelectorAll(".kh-f-card-out");
+		// let fpop = document.querySelector(".kh-f-popup");
+		// const modal = document.getElementById("modal");
+		// card.forEach((element, index, array) => {
+		// 	let cardIn = element.querySelector(".kh-fund-card");
+		// 	if (index % 2 == 0) {
+		// 		element.style.transform = "scale(0.9)";
+		// 		element.classList.add("kh-f-card-min");
+		// 		cardIn.style.backgroundColor = "#FFF1E0";
+		// 	} else {
+		// 		cardIn.style.backgroundColor = "#FFE0E0";
+		// 	}
+		// 	element.style.transition = "0.8s ease-in-out";
+		// 	element.addEventListener("mouseenter", () => {
+		// 		isMouseOverCard = true;
+		// 	});
+		// 	element.addEventListener("mouseleave", () => {
+		// 		isMouseOverCard = false;
+		// 	});
+		// 	element.addEventListener("mouseover", function(event) {
+		// 		card.forEach((otherCard) => {
+		// 			if (otherCard !== element) {
+		// 				if (otherCard.classList.contains("kh-f-card-min")) {
+		// 					otherCard.style.transform = "scale(0.8)";
+		// 				} else {
+		// 					otherCard.style.transform = "scale(0.9)";
+		// 				}
+		// 				otherCard.style.filter = "grayscale(100%)";
+		// 			}
+		// 			document.querySelector(".kh-f-mousemove > img").src =
+		// 				"/resources/img/viewbtn.png";
+		// 			mouseicn.classList.remove("kh-f-none");
+		// 			mouseicn.classList.add("kh-f-block");
+		// 		});
+		// 		if (element.classList.contains("kh-f-card-min")) {
+		// 			this.style.transform = "scale(1)";
+		// 		} else {
+		// 			this.style.transform = "scale(1.1)";
+		// 		}
+		// 		event.stopPropagation();
+		// 	});
+		// 	element.addEventListener("mouseout", function(event) {
+		// 		card.forEach((otherCard) => {
+		// 			if (otherCard !== element) {
+		// 				if (otherCard.classList.contains("kh-f-card-min")) {
+		// 					otherCard.style.transform = "scale(0.9)";
+		// 				} else {
+		// 					otherCard.style.transform = "scale(1)";
+		// 				}
+		// 				otherCard.style.filter = "none";
+		// 			}
+		// 		});
+		// 		if (element.classList.contains("kh-f-card-min")) {
+		// 			this.style.transform = "scale(0.9)";
+		// 		} else {
+		// 			this.style.transform = "scale(1)";
+		// 		}
+		// 		event.stopPropagation();
+		// 	});
+		//
+		// 	element.addEventListener("click", function(event) {
+		// 		let cardParent = event.target.closest(".kh-fund-card");
+		// 		element.classList.add("kh-f-rotate");
+		// 		if (cardParent) {
+		// 			let wlno = event.target.closest(".kh-fund-card").getAttribute("value");
+		// 			let selectobj = jsoninfos.find(function(element) {
+		// 				return element.wl_no == wlno;
+		// 			});
+		// 			console.log(selectobj)
+		// 			if (selectobj.percent === undefined) {
+		// 				percent = 0;
+		// 			}
+		// 			else {
+		// 				percent = selectobj.percent;
+		// 			};
+		// 			document.querySelector("#kh-f-price").innerHTML = percent;
+		// 			document.querySelector("#kh-f-product").innerHTML = selectobj.wl_product;
+		// 			document.querySelector(".kh-f-btn").setAttribute("value", selectobj.wl_no);
+		// 			document.querySelector("#kh-f-img").src = "/resources/img/" + selectobj.wl_product + ".png";
+		// 			openModal()
+		// 			document.querySelector(".kh-f-mousemove > img").src =
+		// 				"/resources/img/backbtn.png";
+		// 			fpop.classList.add("kh-f-rotate");
+		// 		}
+		// 		scrollTimeout = setTimeout(() => {
+		// 			element.classList.remove("kh-f-rotate");
+		// 			fpop.classList.remove("kh-f-rotate");
+		// 		}, 1000);
+		// 		event.stopPropagation();
+		// 	});
+		// });
+
+		// 카드 스타일 초기화
 		let card = document.querySelectorAll(".kh-f-card-out");
-		let fpop = document.querySelector(".kh-f-popup");
-		const modal = document.getElementById("modal");
-		card.forEach((element, index, array) => {
+		card.forEach((element, index) => {
 			let cardIn = element.querySelector(".kh-fund-card");
-			if (index % 2 == 0) {
+			if (index % 2 === 0) {
 				element.style.transform = "scale(0.9)";
 				element.classList.add("kh-f-card-min");
-				cardIn.style.backgroundColor = "#FFF1E0";
+				cardIn.style.background = "#FFF1E0"
 			} else {
-				cardIn.style.backgroundColor = "#FFE0E0";
+				cardIn.style.background = "#FFE0E0";
 			}
-			element.style.transition = "0.8s ease-in-out";
-			element.addEventListener("mouseenter", () => {
-				isMouseOverCard = true;
-			});
-			element.addEventListener("mouseleave", () => {
-				isMouseOverCard = false;
-			});
-			element.addEventListener("mouseover", function(event) {
-				card.forEach((otherCard) => {
-					if (otherCard !== element) {
-						if (otherCard.classList.contains("kh-f-card-min")) {
-							otherCard.style.transform = "scale(0.8)";
-						} else {
-							otherCard.style.transform = "scale(0.9)";
-						}
-						otherCard.style.filter = "grayscale(100%)";
-					}
-					document.querySelector(".kh-f-mousemove > img").src =
-						"/resources/img/viewbtn.png";
-					mouseicn.classList.remove("kh-f-none");
-					mouseicn.classList.add("kh-f-block");
-				});
-				if (element.classList.contains("kh-f-card-min")) {
-					this.style.transform = "scale(1)";
-				} else {
-					this.style.transform = "scale(1.1)";
-				}
-				event.stopPropagation();
-			});
-			element.addEventListener("mouseout", function(event) {
-				card.forEach((otherCard) => {
-					if (otherCard !== element) {
-						if (otherCard.classList.contains("kh-f-card-min")) {
-							otherCard.style.transform = "scale(0.9)";
-						} else {
-							otherCard.style.transform = "scale(1)";
-						}
-						otherCard.style.filter = "none";
-					}
-				});
-				if (element.classList.contains("kh-f-card-min")) {
-					this.style.transform = "scale(0.9)";
-				} else {
-					this.style.transform = "scale(1)";
-				}
-				event.stopPropagation();
-			});
+			element.style.transition = "0.8s ease-in-out"
+		});
+	}
+});
 
-			element.addEventListener("click", function(event) {
-				let cardParent = event.target.closest(".kh-fund-card");
-				element.classList.add("kh-f-rotate");
-				if (cardParent) {
-					let wlno = event.target.closest(".kh-fund-card").getAttribute("value");
-					let selectobj = jsoninfos.find(function(element) {
-						return element.wl_no == wlno;
-					});
-					console.log(selectobj)
-					if (selectobj.percent === undefined) {
-						percent = 0;
-					}
-					else {
-						percent = selectobj.percent;
-					};
-					document.querySelector("#kh-f-price").innerHTML = percent;
-					document.querySelector("#kh-f-product").innerHTML = selectobj.wl_product;
-					document.querySelector(".kh-f-btn").setAttribute("value", selectobj.wl_no);
-					document.querySelector("#kh-f-img").src = "/resources/img/" + selectobj.wl_product + ".png";
-					openModal()
-					document.querySelector(".kh-f-mousemove > img").src =
-						"/resources/img/backbtn.png";
-					fpop.classList.add("kh-f-rotate");
-				}
-				scrollTimeout = setTimeout(() => {
-					element.classList.remove("kh-f-rotate");
+		const cardCon = document.querySelector(".kh-f-card-container");
+
+		// 이벤트 위임으로 모든 카드에 이벤트 리스너 적용
+		cardCon.addEventListener("click", function (event) {
+			const card = event.target.closest(".kh-fund-card");
+
+			if (card) {
+				let wlno = card.getAttribute("value");
+				let selectobj = jsoninfos.find(function (element) {
+					return element.wl_no == wlno;
+				});
+
+				console.log(selectobj);
+
+				let percent = selectobj.percent || 0;
+				document.querySelector("#kh-f-price").innerHTML = percent;
+				document.querySelector("#kh-f-product").innerHTML = selectobj.wl_product;
+				document.querySelector(".kh-f-btn").setAttribute("value", selectobj.wl_no);
+				document.querySelector("#kh-f-img").src = "/resources/img/" + selectobj.wl_product + ".png";
+
+				openModal();
+				document.querySelector(".kh-f-mousemove > img").src = "/resources/img/backbtn.png";
+
+				const fpop = document.querySelector(".kh-f-popup");
+				fpop.classList.add("kh-f-rotate");
+
+				setTimeout(() => {
 					fpop.classList.remove("kh-f-rotate");
 				}, 1000);
+			}
+		})
+
+		// 이벤트 위임으로 마우스 오버 및 아웃 이벤트 처리
+		cardCon.addEventListener("mouseover", function (event) {
+			const card = event.target.closest(".kh-f-card-out");
+
+			if (card) {
+				card.style.transform = card.classList.contains("kh-f-card-min") ? "scale(1)" : "scale(1.1)";
+				card.style.filter = "none";
 				event.stopPropagation();
-			});
+			}
 		});
+
+		cardCon.addEventListener("mouseout", function (event) {
+			const card = event.target.closest(".kt-f-card-out");
+
+			if (card) {
+				card.style.transform = card.classList.contains("kh-f-card-min") ? "scale(0.9)" : "scale(1)";
+				card.style.filter = "grayscale(100%)";
+				event.stopPropagation();
+			}
+		})
+
 		//모달 클로즈 이벤트
+		const modal = document.getElementById("modal");
+		const fpop = document.querySelector(".kh-f-popup");
+
 		modal.addEventListener("click", function(event) {
 			if (!fpop.contains(event.target)) {
 				closeModal();
@@ -138,12 +210,13 @@ $.ajax({
 				event.stopPropagation();
 			}
 		});
+
 		//통계 모달 클로즈 이벤트
 		let statispop = document.querySelector(".kh-f-statistic-pop");
 		const statisticModal = document.getElementById("statisticModal");
 		statisticModal.addEventListener("click", function(event) {
 			if (!statispop.contains(event.target)) {
-				//closeStatistic()
+				closeStatistic()
 			}
 		});
 
@@ -155,9 +228,18 @@ $.ajax({
 		});
 
 		//변수 설정
-		const cardCon = document.querySelector(".kh-f-card-container");
 		const cardConAll = document.querySelectorAll(".kh-f-card-container");
 		const mouseicn = document.querySelector(".kh-f-mousemove");
+
+		// 컨테이너 내용을 복제하여 무한 스크롤 구현
+		const cards = Array.from(cardCon.children);
+		const cardCount = cards.length;
+
+		// 컨텐츠 복제를 두 번 이상으로 확장하여 스크롤 간격을 줄임
+		for (let i = 0; i < 3; i++) {
+			cards.forEach(card => cardCon.appendChild(card.cloneNode(true)))
+		}
+
 		//마우스 따라다니는 요소
 		cardCon.addEventListener("mouseover", function(event) {
 			if (event.target === cardCon) {
@@ -169,6 +251,7 @@ $.ajax({
 			mouseicn.classList.add("kh-f-block");
 			event.stopPropagation();
 		});
+
 		document.addEventListener("mousemove", (event) => {
 			const mouseX = event.clientX;
 			const mouseY = event.clientY;
@@ -176,41 +259,66 @@ $.ajax({
 			mouseicn.style.left = mouseX + "px";
 			mouseicn.style.top = mouseY + "px";
 		});
+
 		cardCon.addEventListener("mouseout", function() {
 			mouseicn.classList.remove("kh-f-block");
 			mouseicn.classList.add("kh-f-none");
 		});
 
-		//마우스 휠할때 도는 애니메이션
-		cardCon.addEventListener("wheel", function(event) {
-			if (isMouseOverCard) return;
-			card.forEach((element) => {
-				// Prevent the default vertical scroll
-				event.preventDefault();
-				// Scroll horizontally
-				const scrollSpeed = 0.5;
-				this.scrollLeft += event.deltaY * scrollSpeed;
-				if (scrollTimeout) {
-					if (element.classList.contains("kh-f-card-min")) {
-						element.style.transform = "scale(0.9) rotateY(30deg)";
-					} else {
-						element.style.transform = "rotateY(30deg)";
-					}
-					clearTimeout(scrollTimeout);
-				}
-				scrollTimeout = setTimeout(() => {
-					card.forEach(function(element) {
-						element.style.transform = "rotateY(0deg)";
-						if (element.classList.contains("kh-f-card-min")) {
-							element.style.transform = "scale(0.9)";
-						} else {
-							element.style.transform = "scale(1)";
-						}
-					});
-					isRotated = false;
-				}, 300); // 1초 동안 유지
-			});
+		const card = cardCon.children[0];
+		const cardWidth = card.offsetWidth; // 카드 너비
+
+		// 간격을 줄이기 위한 추가 스크롤 오프셋
+		const offset = cardWidth / 4; // 카드 너비의 1/4만큼 스크롤을 더하여 간격 줄임
+
+		// 무한 스크롤 구현
+		cardCon.addEventListener("scroll", function () {
+		const scrollWidthHalf = cardCon.scrollWidth / 2;
+
+		if (cardCon.scrollLeft >= scrollWidthHalf) {
+		// 스크롤 위치를 조정하여 간격 줄이기
+		cardCon.scrollLeft -= (scrollWidthHalf - offset);
+			} else if (cardCon.scrollLeft <= 0) {
+		cardCon.scrollLeft += (scrollWidthHalf - offset);
+		}
 		});
+
+		//마우스 휠할때 도는 애니메이션
+		// cardCon.addEventListener("wheel", function(event) {
+		// 	if (isMouseOverCard) return;
+		// 	card.forEach((element) => {
+		// 		// Prevent the default vertical scroll
+		// 		event.preventDefault();
+		// 		// Scroll horizontally
+		// 		const scrollSpeed = 0.5;
+		// 		this.scrollLeft += event.deltaY * scrollSpeed;
+		// 		if (scrollTimeout) {
+		// 			if (element.classList.contains("kh-f-card-min")) {
+		// 				element.style.transform = "scale(0.9) rotateY(30deg)";
+		// 			} else {
+		// 				element.style.transform = "rotateY(30deg)";
+		// 			}
+		// 			clearTimeout(scrollTimeout);
+		// 		}
+		// 		scrollTimeout = setTimeout(() => {
+		// 			card.forEach(function(element) {
+		// 				element.style.transform = "rotateY(0deg)";
+		// 				if (element.classList.contains("kh-f-card-min")) {
+		// 					element.style.transform = "scale(0.9)";
+		// 				} else {
+		// 					element.style.transform = "scale(1)";
+		// 				}
+		// 			});
+		// 			isRotated = false;
+		// 		}, 300); // 1초 동안 유지
+		// 	});
+		// });
+
+		cardCon.addEventListener("wheel", function (event) {
+			event.preventDefault();
+			const scrollSpeed = 0.5;
+			cardCon.scrollLeft += event.deltaY * scrollSpeed;
+		})
 
 		let isDown = false;
 		let startX;
@@ -259,10 +367,8 @@ $.ajax({
 			cardCon.scrollLeft = scrollLeft - walk;
 		});
 
-	},
-});//ajax 종료
+//ajax 종료
 //크기,색상 호버시 확대 및 다른요소 비활성화
-
 
 function goStatistic(wlno) {
 	const container = document.querySelector("#kh-input-box");
@@ -315,15 +421,22 @@ function goStatistic(wlno) {
 				});
 
 				openStatistic();
-				statisticModal.querySelector('.kh-f-statistic-name > div:nth-child(1)').classList.remove('kh-f-none');
-				let statisBars = statisticModal.querySelectorAll('.kh-f-statistic-abled-bar')
-				statisBars.forEach((element) => {
+				// statisticModal.querySelector('.kh-f-statistic-name > div:nth-child(1)').classList.remove('kh-f-none');
+				// let statisBars = statisticModal.querySelectorAll('.kh-f-statistic-abled-bar')
+				// statisBars.forEach((element) => {
+				// 	element.style.width = element.getAttribute("data-value") + "%";
+				// });
+				// let statisNumber = statisticModal.querySelectorAll('.kh-f-statistic-percent > h1 > span')
+				// statisNumber.forEach((element) => {
+				// 	animateValue(element, 0, parseInt(element.getAttribute("data-value")), 1000);
+				// });
+
+				// 복제된 요소도 포함하여 초기화 작업 수행
+				let staticBars = document.querySelectorAll('.kh-f-statistic-abled-bar');
+				staticBars.forEach((element) => {
 					element.style.width = element.getAttribute("data-value") + "%";
 				});
-				let statisNumber = statisticModal.querySelectorAll('.kh-f-statistic-percent > h1 > span')
-				statisNumber.forEach((element) => {
-					animateValue(element, 0, parseInt(element.getAttribute("data-value")), 1000);
-				});
+
 				//카운트다운
 				let countDown = document.getElementById('finalCount');
 				let count = 10;
