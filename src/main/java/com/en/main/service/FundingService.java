eca193af66eac8b26cd1da980927b14d4772361b
yujeongstellakim <yujeongstellakim@gmail.com>
2024-08-22 10:05:50 +0900

    ignore set

--- src/main/java/com/en/main/service/FundingService.java
+++
@@ -1,7 +0,0 @@
-package com.en.main.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FundingService {
-}
--- src/main/resources/static/js/home/home.js
+++
@@ -1,6 +0,0 @@
-function changeColorNRedirect(button) {
-    button.classList.add('active');  // active 상태를 강제로 추가
-    setTimeout(function() {
-        location.href = '/main';
-    }, 300);
-}
